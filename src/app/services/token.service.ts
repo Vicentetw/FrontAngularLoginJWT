f21d31f45c5f672fd9f876747387d6c188fa668f
Vicentetw <perrottavicente@gmail.com>
2022-10-24 15:12:20 -0300

    token service and auth service

--- src/app/services/token.service.ts
+++ src/app/services/token.service.ts
@@ -16,5 +16,30 @@ export class TokenService {
     window.sessionStorage.removeItem(TOKEN_KEY);
     window.sessionStorage.setItem(TOKEN_KEY,token)
   }
-
+ public getToken():string{
+  return sessionStorage.getItem(TOKEN_KEY)!;
+ }
+ public setUserName(userName:string):void{
+  window.sessionStorage.removeItem(USERNAME_KEY);
+  window.sessionStorage.setItem(USERNAME_KEY,userName)
+}
+public getUserName():string{
+return sessionStorage.getItem(USERNAME_KEY)!;
+}
+public setAuthorities(Authorities:string[]):void{
+  window.sessionStorage.removeItem(AUTHORITIES_KEY);
+  window.sessionStorage.setItem(AUTHORITIES_KEY,JSON.stringify(Authorities));
+}
+public getAuthorities(): string[]{
+  this.roles = [];
+  if(sessionStorage.getItem(AUTHORITIES_KEY)){
+    JSON.parse(sessionStorage.getItem(AUTHORITIES_KEY)!).forEach((authority:any) => {
+      this.roles.push(authority.authority);
+    });
+  }
+  return this.roles;
+}
+public logOut(): void{
+  window.sessionStorage.clear();
+}
 }
