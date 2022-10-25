f899096bebe8dbaa92b2c9412a9f534fd1e5fa65
Vicentetw <perrottavicente@gmail.com>
2022-10-25 08:29:31 -0300

    login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FrontAngularLoginJWT';
+  
+  public loginValid = true;
+  public username = '';
+  public password = '';
+  
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatSliderModule } from '@angular/material/slider';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToolbarComponent } from './shared/components/toolbar/toolbar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ToolbarModule } from './shared/components/toolbar/toolbar.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    NgbModule,
+    ToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
